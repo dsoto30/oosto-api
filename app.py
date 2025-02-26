d9f770910a28f5e282b315723e3d67dbc2e107ab
Daniel Soto <sotodaniel531@gmail.com>
2025-02-26 13:23:01 -0800

    added db connection to client script and added functionality to create a unkown detection in guest group

--- app.py
+++ app.py
@@ -3,8 +3,31 @@ import socketio
 import threading
 import os
 from dotenv import load_dotenv
+import pyodbc
 
 load_dotenv()
+
+
+
+
+DB_SERVER = os.getenv("DB_SERVER")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+conn_str = (
+    'DRIVER={ODBC Driver 18 for SQL Server};'
+    f'SERVER={DB_SERVER};'
+    f'DATABASE={DB_NAME};'
+    f'UID={DB_USER};'
+    f'PWD={DB_PASSWORD};'
+    'TrustServerCertificate=yes;'
+)
+
+conn = pyodbc.connect(conn_str)
+cursor = conn.cursor()
+print("connected to db")
 
 
 SERVER_IP = os.getenv("SERVER_IP")
@@ -56,31 +79,58 @@ def create_socket(token):
 
         if unknown create subject through OOSTO in guest group
         """
-        print(f"processing {len(track_data)} tracks.")
-        track = track_data[0]
-        closest_match_score = 0 if track['closeMatches'] is None else find_closest_match(track['closeMatches'])
-
+        print(f"Got {len(track_data)} detections!")
         
+        imageQualityThreshold = 80
+        closestMatchScoreThreshold = .50
+        guest_group_id = "ee87c48a-146c-4a0d-820a-df4c0cacb41d"
 
-        new_track = {"frameId": track["id"],"time_recognized": track["frameTimeStamp"], "imageQualityScore": track["landmarkScore"], "cameraTitle": track['camera']['title'], "cameraGroupId": track['camera']['cameraGroupId'], "closestScore": closest_match_score}
-        print(new_track)
+        track = track_data[0]
+        # print(track)
 
-    @sio.on('recognition:created')
-    def recognition_created(recognition_data):
+        if track["landmarkScore"] >= imageQualityThreshold:
+            closest_match_score = 0 if not track['closeMatches'] else find_closest_match(track['closeMatches'])
+            
+            if closest_match_score <= closestMatchScoreThreshold:
+                body_request = {
+                    "name": "John Doe",
+                    "description": "",
+                    "groups": [guest_group_id],
+                    "sendToHq": False,
+                    "searchBackwards": [{
+                        "searchBackwardsThreshold": .6,
+                        "objectType": track["objectType"]
+                    }],
+                    "images": [
+                        {"isPrimary": True, "landmarkScore": track['landmarkScore'], "objectType": track['objectType'], "url": track['images'][0]['url'], "track": {
+                            "camera": track['camera']['id'], "frameTimeStamp": track['frameTimeStamp'], "id": track['id']
+                        }}
+                        ]
+                    
+                }
 
-        """
-        Give group_id of camera of recognition check if it's from entrance group If not ignore.
+                response = requests.post(f"{BASE_API_URL}/subjects/from-track", json=body_request, verify=False, headers=HEADERS)
+                print("added unknown person to guest group!")
 
-        check if subject_id already exists in table if not insert recognition information
 
-        SQL Job to run at 6am daily and COUNT * FROM recognitions table (should be all unique) prune table after
 
-        Most stuff should be done by the UI or API calls
-        """
-        recognition = recognition_data[0]
-        response = {"recognition_id": recognition["id"], "location_recognized": recognition["camera"]["title"], "relatedTrackId": recognition["relatedTrackId"], "subjectScore": recognition["subjectScore"], "matches": recognition["closeMatches"], "name": recognition["subject"]["name"]}
 
-        print(f"{response["name"]} was recognized from {response['location_recognized']}, with a score of {response['subjectScore']} ")
+    # @sio.on('recognition:created')
+    # def recognition_created(recognition_data):
+
+    #     """
+    #     Give group_id of camera of recognition check if it's from entrance group If not ignore.
+
+    #     check if subject_id already exists in table if not insert recognition information
+
+    #     SQL Job to run at 6am daily and COUNT * FROM recognitions table (should be all unique) prune table after
+
+    #     Most stuff should be done by the UI or API calls
+    #     """
+    #     recognition = recognition_data[0]
+    #     response = {"recognition_id": recognition["id"], "location_recognized": recognition["camera"]["title"], "relatedTrackId": recognition["relatedTrackId"], "subjectScore": recognition["subjectScore"], "matches": recognition["closeMatches"], "name": recognition["subject"]["name"]}
+
+    #     print(f"{response["name"]} was recognized from {response['location_recognized']}, with a score of {response['subjectScore']} ")
 
 
     def create_connection(token):
